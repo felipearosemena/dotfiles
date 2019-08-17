c4870bb869bfc162aaccba095dd625f2a441cc19
Luis Felipe Arosemena <luisfelipearosemena@Luiss-MacBook-Pro.local>
2019-08-17 12:46:45 -0400

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+# zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# chsh -s $(which zsh)
+
+
+# Homebrew
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Node
+# brew install node
+# brew install nvm
+
+# Yarn
+# brew install yarn
+
+# React Native
+# brew install node
+# brew install watchman
+# brew tap AdoptOpenJDK/openjdk
+# brew cask install adoptopenjdk8
+# yarn global add react-native-cli
+
+# Android SDK
+
+# Accept the licenses!
+# cd ~/Library/Android/sdk/tools/bin
+# ./sdkmanager --licenses
+
+# Keystore file
